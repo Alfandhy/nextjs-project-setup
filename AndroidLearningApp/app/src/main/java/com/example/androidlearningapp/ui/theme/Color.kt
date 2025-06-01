4ceb380b2f52248dc5119d7cfdcfdee0935719a2
Alfandhy <43575575+Alfandhy@users.noreply.github.com>
2025-06-01 10:51:29 +0800

    Add AndroidLearningApp/app/src/main/java/com/example/androidlearningapp/ui/theme/Color.kt 1748746289621

---
+++ AndroidLearningApp/app/src/main/java/com/example/androidlearningapp/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.androidlearningapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF0B4F6C)
+val AccentGreen = Color(0xFF61CE70)
+val LightBlue = Color(0xFF01BAEF)
+val DarkGray = Color(0xFF333333)
+val White = Color(0xFFFFFFFF)
